a9dbea978db8338b8b12d4138c817d4a423aee0b
emonahmed123 <imonshomon@gmail.com>
2023-02-01 21:00:07 +0600

    change

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -100,7 +100,7 @@ catch(error){
    
 
   }
-  
+
   
 
 
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,13 +1,16 @@
 const express =require("express");
 const router= express.Router();
 const userController = require("../controllers/user.controller")
-
-
+const verifyToken =require("../middleware/verifyToken")
+const authorization=require("../middleware/authorization")
 
 
 router.post("/signup",userController.signup)
 
 router.post("/login", userController.login);
 
-router.get("/allm", userController.alluser);
+router.get("/allm",verifyToken, authorization('admin'),userController.alluser);
+
+
+
 module.exports = router;
